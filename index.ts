e9f9815bbf0dd59561f0ca58220fdeff16b105dd
sean <insanityseanboy@gmail.com>
2023-09-19 23:45:46 +1000

    initial commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "InstaView",
+    description: "Allows Instagram videos to be previewed",
+    authors: [{
+        name: "Wiiiiam and Sean",
+        id: 0n
+    }],
+
+    patches: [],
+    start() {
+
+    },
+    stop() {
+
+    },
+});
