cc8b4dc1b03a486fe1966a6c0e3932b0bb647718
sean <1931943+insanityseanboy@users.noreply.github.com>
2023-09-25 14:50:38 +1000

    made 1 possible control entry, sep names, updated desc. ready for logic

--- index.ts
+++ index.ts
@@ -1,15 +1,34 @@
 import definePlugin from "@utils/types";
 
-export default definePlugin({
-    name: "InstaView",
-    description: "Allows Instagram videos to be previewed",
-    authors: [{
-        name: "Wiiiiam and Sean",
-        id: 0n
-    }],
+async function doTheThing(this: Component<Props>)
+{
+    try {
+        // something like "if its not an instagram link or has already been dealt with, bail now"
+        const { embed } = this.props;
+        if (!embed || embed.provider?.name !== "Instagram" || embed.video?.url) return;
 
-    patches: [],
-    // these may not be necessary
-    start() {},
-    stop() {},
+        // something like "get the video source from instagram url", and put it in the embed modal
+        // I don't know how to do this lol
+
+        //this.forceUpdate(); // not sure if necessary, we'll find out
+    } catch (err) {
+        new Logger("InstaView_doTheThing").error("Exception: ", err);
+    }
+}
+
+export default definePlugin
+({
+    name: "InstaView",
+    description: "Allows Instagram videos to be embedded",
+    authors: [{name: ["Wiiiiam"],id: 0n},{name: ["Sean"],id: 0n}],
+
+    patches: 
+    [{
+        find: "this.renderInlineMediaEmbed",
+        replacement: 
+            {
+                match: /(\i).render=function.{0,50}\i\.embed/,
+                replace: "$1.componentDidMount=$self.doTheThing,$&"
+            }
+    }]
 });
